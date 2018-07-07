40b2f261aaddb2dfbd60ced437c41159b36b9fda
Musaddiq <musaddiqktk@gmail.comm>
2018-07-08 02:21:41 +0500

    Branch diff testing

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
  * NOTE: If you change these, also change the error_reporting() code below
  *
  */
- //Test5 check commit and then merge it with master
+ //Branch diff testing check commit and then merge it with master
 	define('ENVIRONMENT', 'testing');
 /*
  *---------------------------------------------------------------
